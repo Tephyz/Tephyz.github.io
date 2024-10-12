97e050cab039d79404d830cde652013fb1f7a568
Tephyz <158828331+Tephyz@users.noreply.github.com>
2024-10-12 18:35:44 +0800

    Deploy

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({pages: "build", assets: "build", fallback: undefined,precompress: false,strict: true}),
+    adapter: adapter({pages: "build", assets: "build", fallback: 'index.html',precompress: false,strict: true}),
   },
   preprocess: vitePreprocess()
 };
